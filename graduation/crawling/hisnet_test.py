f15b30e648e2e4bfeed7db9c8de33c8ffd48bf2c
jamie <rmk1075@gmail.com>
2019-12-19 21:32:06 +0900

    database_create_students_and_update_students

--- graduation/crawling/hisnet_test.py
+++ graduation/crawling/hisnet_test.py
@@ -4,6 +4,7 @@
 ## select the data
 
 import sys
+import datetime
 import pymysql
 import requests
 from bs4 import BeautifulSoup as bs
@@ -11,7 +12,8 @@ from selenium import webdriver
 from selenium.webdriver.common.alert import Alert
 
 def crawling(hisnet):
-    grade_list = [['학기', '과목코드', '과목명', '이수구분', '학점', '성적', '평점']]
+    #grade_list = [['학기', '과목코드', '과목명', '이수구분', '학점', '성적', '평점']]
+    grade_list = []
 
     num = 2
     table = hisnet.find_element_by_xpath('//*[@id="att_list"]')
@@ -37,21 +39,23 @@ def crawling(hisnet):
         except:
             break
 
-    lecture_count = 0 # 총 이수 과목 수
-    general = 0.0 # 총 이수 교양 학점
-    major = 0.0 # 총 이수 전공 학점
-    else_list = [] # 기타 과목들
-    for lecture in grade_list:
-        #TODO: if grade F, ignore?
-        #TODO: if senior, 공동체리더십훈련 - 0.5 point -> changed to 1????
-        if '교양' in lecture[3] or '실무' in lecture[3]:
-            lecture_count += 1
-            general += float(lecture[4])
-        elif '전공' in lecture[3]:
-            major += float(lecture[4])
-            lecture_count += 1
-        else:
-            else_list.append([lecture[2], lecture[3]])
+    return grade_list
+
+    # lecture_count = 0 # 총 이수 과목 수
+    # general = 0.0 # 총 이수 교양 학점
+    # major = 0.0 # 총 이수 전공 학점
+    # else_list = [] # 기타 과목들
+    # for lecture in grade_list:
+    #     #TODO: if grade F, ignore?
+    #     #TODO: if senior, 공동체리더십훈련 - 0.5 point -> changed to 1????
+    #     if '교양' in lecture[3] or '실무' in lecture[3]:
+    #         lecture_count += 1
+    #         general += float(lecture[4])
+    #     elif '전공' in lecture[3]:
+    #         major += float(lecture[4])
+    #         lecture_count += 1
+    #     else:
+    #         else_list.append([lecture[2], lecture[3]])
 
         # print(lecture)
 
@@ -114,23 +118,47 @@ if __name__ == "__main__":
     conn = pymysql.connect(host='localhost', user='root', password='jjmin100705!', db='mydb', charset='utf8')
     curs = conn.cursor()
 
-    query = 'select * from lastaccess where hisnetID=\''+LOGIN_INFO['id']+'\';'
+    query = 'select * from lastupdate where hisnetID=\''+LOGIN_INFO['id']+'\';'
     curs.execute(query)
 
     access = curs.fetchall()
+    lecture_list = []
     # TODO: compare lastaccess with current date -> update the date
     if access == ():
         # crawling
-        crawling(hisnet)
+        lecture_list = crawling(hisnet)
 
-        # insert new data to lastaccess
-        query = 'insert into lastaccess (hisnetID, lastAccess) values (\''+LOGIN_INFO['id']+'\', NOW());'
-        curs.execute(query)
-        conn.commit()
+        # insert new lecture data to students
+        try:
+            for lecture in lecture_list:
+                query = 'insert into students values (%s, %s, %s, %s, %s, %s, %s, %s);'
+                val = (LOGIN_INFO['id'], lecture[0], lecture[1], lecture[2], lecture[3], float(lecture[4]), lecture[5], float(lecture[6]))
+                curs.execute(query, val)
+                conn.commit()
+
+            # insert new data to lastupdate
+            try:
+                query = 'insert into lastupdate (hisnetID, lastAccess) values (\'' + LOGIN_INFO['id']+'\, NOW());'
+                curs.execute(query)
+                conn.commit()
+            except:
+                print('error during update the lastupdate')
+        
+        except:
+            query = 'delete from students where id=\''+LOGIN_INFO['id']+'\';'
+            curs.execute(query)
+            conn.commit()
+            print('error during update the lecture list')
+
+    # TODO: update when the last update is over the 30 days ago
+    elif datetime.timedelta(days=30) < datetime.datetime.today() - access[0][1]:
+        print('has to be updated')
     else:
         # select from DB
-        print(access)
-        print(access[0][0])
-        print(access[0][1])
+        query = 'select * from students where id=\''+LOGIN_INFO['id']+'\';'
+        curs.execute(query)
+        lecture_list = curs.fetchall()
+
+        print(lecture_list)
 
     conn.close()
