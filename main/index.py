0426765cc1dd0346392bbec9c0e989292aefd55f
jamie <rmk1075@gmail.com>
2020-01-22 00:42:56 +0900

    main_index

---
+++ main/index.py
@@ -0,0 +1,10 @@
+# main site by flask
+
+# '/'
+# index.html
+
+# '/login'
+# check by hisnet_id, hisnet_pwd
+
+# '/table/'+hisnet_id # is it possible?
+# prints out the information from hisnet by the table
